b69b20a793c86213fbe90890c5dc14d56f4d4514
susmitha <susmitha.matturi44@gmail.com>
2020-10-20 10:20:02 +0000

    added

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 //this program will give a simple message//
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+    System.out.println("Hello aaryan welcome to git"); 
+    System.out.println("Hello Java");  
     }  
 }  
